89dd1963692f2cc85c185e2b067ea0d58fd26ad1
amr ismayl <105287289+amrIsmayl@users.noreply.github.com>
2022-10-11 17:24:54 +0200

    The first exam in backend, a library project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express()
+const port = 3000
+
+
+app.use(express.json());
+
+
+
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/sara7a')
+    .then(() => {
+        console.log("database connented");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
